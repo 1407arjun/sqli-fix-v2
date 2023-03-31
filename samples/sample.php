e4918d2d5151ca1be244a4479719573393e13d24
Arjun Sivaraman <arjun140702@gmail.com>
2023-03-31 14:32:12 +0530

    Add sample file

---
+++ samples/sample.php
@@ -0,0 +1,40 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM orders WHERE order_date >= '$start_date' AND order_total < $max_total AND order_status = $status AND order_id IN ($order_ids) AND order_amount BETWEEN :min_amount AND $max_amount AND customer_id = $customer_id AND order_is_active = $is_active";
+
+$sql = "INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$sql = "INSERT INTO '$wpdb->guest' (firstname, lastname) VALUES ('John', 'Doe')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$name = $_POST['name'];
+$conn->query("SELECT * FROM '$wpdb->myTable' WHERE name like '$name'");
+
+$age = $_POST['age'];
+$sessionId = $_SESSION['id'];
+$conn->query("UPDATE '$wpdb->myTable' SET age = $age, isLogin = TRUE WHERE id = '$sessionId'");
+
+$conn->close();
+?>
