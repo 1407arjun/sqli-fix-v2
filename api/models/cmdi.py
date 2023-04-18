fadcf364f8a3e051e51dfd3dd7a57c8df7729017
Arjun Sivaraman <arjun140702@gmail.com>
2023-04-18 15:57:25 +0530

    Add command injection detection

---
+++ api/models/cmdi.py
@@ -0,0 +1,25 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+import pandas as pd
+
+df = pd.read_csv("./data/cmdi/cmdi_dataset.csv", encoding='utf-8')
+df['Sentence'] = df['Sentence'].apply(lambda x: str(x).lower())
+vectorizer = CountVectorizer(analyzer='word', ngram_range=(1, 2))
+X = vectorizer.fit_transform(df['Sentence'].values.astype('U')).toarray()
+y = df['Label']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+nb_clf = GaussianNB()
+nb_clf.fit(X_train, y_train)
+
+def predict(ip):
+    #injected_query = query.replace(variable, ip)
+    features = vectorizer.transform([ip]).toarray()
+    return nb_clf.predict(features)[0]
+
+def accuracy():
+    y_pred = nb_clf.predict(X_test)
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+    print("Precision:", precision_score(y_test, y_pred))
+    print("Recall:", recall_score(y_test, y_pred))
